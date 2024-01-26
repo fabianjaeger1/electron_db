8a8152a6b30ac331fe6229be2da7467e5e927124
fabianjaeger1 <fabian_jaeger@outlook.com>
2024-01-26 10:42:53 +0100

    init commit

---
+++ main.js
@@ -0,0 +1,58 @@
+const { app, BrowserWindow , ipcMain} = require('electron');
+const path = require('path');
+const url = require('url');
+
+let mainWindow;
+
+function deleteText() {
+    var textField = document.getElementById('searchInput');
+
+    // Check if the current value is not the placeholder
+    if (textField.value !== textField.placeholder) {
+      textField.value = ''; // Clear the text field
+    }
+}
+    
+function createWindow() {
+    mainWindow = new BrowserWindow({
+        width: 1200,
+        height: 800,
+        // resizable: false,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js'),
+            nodeIntegration: true
+        }
+    });
+
+    mainWindow.loadFile('index.html');
+}
+
+// ipcMain.on('change-view', ()=>{
+//     BrowserWindow.getAllWindows()[0].loadURL(url.format({
+//         pathname : path.join(__dirname,'settings.html'),
+//         protocol:'file',
+//         slashes:true
+//     }));
+// });
+
+app.whenReady().then(() => {
+    createWindow();
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow();
+        }
+    
+    ipcMain.on('load-second-page', () => {
+        console.log("loading second page")
+        // Load the second internal page
+        mainWindow.loadFile(path.join(__dirname, 'settings.html'));
+        });
+    });
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
---
+++ preload.js
@@ -0,0 +1,14 @@
+const { contextBridge, ipcRenderer} = require('electron/renderer')
+    contextBridge.exposeInMainWorld('myAPI', {
+    desktop: true
+})
+
+window.addEventListener('DOMContentLoaded', () => {
+    const replaceText = (selector, text) => {
+        const element = document.getElementById(selector)
+            if (element) element.innerText = text
+    }
+    for (const dependency of ['chrome', 'node', 'electron']) {
+        replaceText(`${dependency}-version`, process.versions[dependency])
+    }
+})
