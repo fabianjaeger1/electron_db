dfe648dbe7e88a936e36b3a8b376b70e8f221a73
Max Kapustin <maxkapustin999@gmail.com>
2024-01-26 16:59:00 +0100

    DRAFT Add search endpoint

--- backend/main.py
+++ backend/main.py
@@ -13,9 +13,10 @@ def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/search/{query}")
+def search(query: int):
+    # TODO: Add Vector DB Search query
+    return {"query": query}
 
 
 @app.get("/shutdown")
